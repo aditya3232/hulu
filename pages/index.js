ba97be36f319908f651b19b363ef0344f763c83d
aditya3232 <m.aditya3232@gmail.com>
2021-04-29 12:26:12 +0700

    lanjut komponen
    - buat komponen Results
    - buat komponen Thumbnail

--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,10 @@
 import Head from 'next/head';
 import Header from '../components/Header';
 import Nav from '../components/Nav';
+import Results from '../components/Results';
+import requests from '../utils/requests';
 
-export default function Home() {
+export default function Home({ results }) {
   return (
     <div>
       <Head>
@@ -11,13 +13,23 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      {/* Header */}
       <Header />
-
-      {/* Nav */}
       <Nav />
-
-      {/* Result */}
+      <Results results={results} />
     </div>
   );
+}
+
+// server side rendering di next.js
+// server render dilakukan dlu, baru client render
+export async function getServerSideProps(context) {
+  const genre = context.query.genre;
+
+  const request = await fetch(`https://api.themoviedb.org/3${requests[genre]?.url || requests.fetchTrending.url}`).then((res) => res.json());
+
+  return {
+    props: {
+      results: request.results,
+    },
+  };
 }
